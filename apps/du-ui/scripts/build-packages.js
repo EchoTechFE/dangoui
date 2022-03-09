06405e7703de9da650c641b8598408f71f0063f5
lbb00 <mr@lbb00.com>
2022-03-09 15:05:15 +0800

    feat: du-ui build packages

---
+++ apps/du-ui/scripts/build-packages.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const fs = require('fs');
+
+const dependencies = require('../package.json').dependencies;
+
+const duDeps = Object.keys(dependencies).filter((dep) =>
+  /@echoingtech\/du-(?!styles)/.test(dep),
+);
+
+if (!fs.existsSync(path.join(__dirname, '../packages'))) {
+  fs.mkdirSync(path.join(__dirname, '../packages'));
+}
+
+duDeps.map((dep) => {
+  const modulesSrcDir = path.join(__dirname, '../node_modules', dep, '/src');
+  const files = fs.readdirSync(modulesSrcDir);
+
+  const packageDir = path.join(
+    __dirname,
+    '../packages',
+    dep.replace('@echoingtech/', ''),
+  );
+
+  if (!fs.existsSync(packageDir)) {
+    fs.mkdirSync(packageDir);
+  }
+  files.map((file) => {
+    const fileData = fs.readFileSync(path.join(modulesSrcDir, file));
+    fs.writeFileSync(path.join(packageDir, file), fileData);
+  });
+});
