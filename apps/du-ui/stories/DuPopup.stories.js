1036ba908a58796236344205c022007f9e17f51a
zhuscat <zhudx6512@163.com>
2022-03-14 11:31:56 +0800

    feat: add simple scroll-view on web

--- apps/du-ui/stories/DuPopup.stories.js
+++ apps/du-ui/stories/DuPopup.stories.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import VueCompositionApi from '@vue/composition-api'
 import { DuPopup } from '@echoingtech/du-popup/src/index.js'
 import DuButton from '@echoingtech/du-button/src/Button.vue'
+import ScrollView from '../components/ScrollView.vue'
 import './theme.scss'
 
 import '@echoingtech/du-styles/styles/index.scss'
 
 Vue.use(VueCompositionApi)
+Vue.component('scroll-view', ScrollView)
 
 export default {
   title: 'Popup',
