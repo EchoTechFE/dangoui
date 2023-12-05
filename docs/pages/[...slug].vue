ea6efa94e5247652c7d1baa3e9ae93d0bfda02fb
zhuscat <zhudx6512@163.com>
2023-12-05 17:10:50 +0800

    feat: add prev, next article navigation

--- docs/pages/[...slug].vue
+++ docs/pages/[...slug].vue
@@ -1,9 +1,33 @@
 <template>
-  <main class="doc-content flex mt-$doc-header-h px-32px items-start">
+  <main class="doc-content mt-$doc-header-h px-32px">
     <ContentDoc
       class="prose prose-neutral flex-1 !max-w-full text-14px"
       :path="$route.path === '/' ? '/get-started/introduction' : $route.path"
     />
+    <div class="grid grid-cols-2 gap-x-16px mb-32px mt-16px">
+      <NuxtLink
+        v-if="surround?.[0]"
+        class="border border-solid border-border-2 transition cursor-pointer hover:border-qd-purple rounded-4px p-8px px-16px flex flex-col"
+        :href="surround?.[0]?._path"
+      >
+        <div class="text-12px c-gray-500 mb-8px">上一篇</div>
+        <div class="c-qd-purple text-14px font-medium">
+          {{ surround?.[0]?.title }}
+        </div>
+      </NuxtLink>
+      <div v-else />
+      <NuxtLink
+        v-if="surround?.[1]"
+        class="border border-solid border-border-2 transition cursor-pointer hover:border-qd-purple rounded-4px p-8px px-16px flex flex-col items-end"
+        :href="surround?.[1]?._path"
+      >
+        <div class="text-12px c-gray-500 mb-8px">下一篇</div>
+        <div class="c-qd-purple text-14px font-medium">
+          {{ surround?.[1]?.title }}
+        </div>
+      </NuxtLink>
+      <div v-else />
+    </div>
   </main>
   <div
     class="doc-outline fixed top-0 right-0 mt-$doc-header-h w-$doc-outline-w text-14px pl-16px border-l border-l-solid border-l-gray-200"
@@ -32,4 +56,13 @@ const { data: md } = await useAsyncData(route.path, () =>
     route.path === '/' ? '/get-started/introduction' : route.path,
   ).findOne(),
 )
+
+const { data: surround } = await useAsyncData(`surround:` + route.path, () => {
+  return queryContent()
+    .only(['_path', 'title'])
+    .where({
+      _partial: false,
+    })
+    .findSurround(route.path === '/' ? '/get-started/introduction' : route.path)
+})
 </script>
