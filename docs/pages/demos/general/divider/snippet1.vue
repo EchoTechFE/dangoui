2e1a61ce40562a945ffb81e63b9a807460cccb81
zhuscat <zhudx6512@163.com>
2023-12-08 22:02:16 +0800

    feat(divider): 完善 divider 文档

---
+++ docs/pages/demos/general/divider/snippet1.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <PreviewBlock title="默认">
+    <DuDivider />
+  </PreviewBlock>
+  <PreviewBlock title="带文字">
+    <DuDivider>分割线</DuDivider>
+  </PreviewBlock>
+  <PreviewBlock title="垂直">
+    <div class="flex items-center gap-x-8px">
+      <DuButton
+        type="text"
+        size="mini"
+        icon="multiselection"
+      >
+        同时搜多款
+      </DuButton>
+      <DuDivider type="vertical" />
+      <DuButton
+        type="text"
+        size="mini"
+        icon="info-circle"
+      >
+        规格说明
+      </DuButton>
+    </div>
+  </PreviewBlock>
+</template>
+
+<script setup lang="ts">
+import { DuDivider, DuButton } from '@frontend/dumpling'
+</script>
--- packages/dumpling/src/divider/Divider.vue
+++ packages/dumpling/src/divider/Divider.vue
@@ -1,6 +1,12 @@
 <template>
   <div :class="['du-divider', `du-divider--${config.type}`]">
-    <slot />
+    <template v-if="config.type === 'horizontal'">
+      <div class="du-divider__left" />
+      <div class="du-divider__content">
+        <slot />
+      </div>
+      <div class="du-divider__right" />
+    </template>
   </div>
 </template>
 
@@ -8,11 +14,26 @@
 import { computed, inject } from 'vue'
 import { TabsInjectionKey } from '../tabs/helpers'
 
+const props = withDefaults(
+  defineProps<{
+    type?: 'horizontal' | 'vertical'
+  }>(),
+  {
+    type: undefined,
+  },
+)
+
 const tabsConfig = inject(TabsInjectionKey)
 
 const config = computed(() => {
+  if (props.type) {
+    return {
+      type: props.type || 'horizontal',
+    } as const
+  }
+
   return {
     type: tabsConfig ? 'vertical' : 'horizontal',
-  }
+  } as const
 })
 </script>
