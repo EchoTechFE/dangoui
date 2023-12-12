cbde5fe1f641155932515ed049c892075a699895
zhuscat <zhudx6512@163.com>
2023-12-12 11:16:20 +0800

    docs: update right slot readme

--- docs/pages/demos/data-display/tabs/snippet3.vue
+++ docs/pages/demos/data-display/tabs/snippet3.vue
@@ -1,7 +1,7 @@
 
 <template>
   <PreviewBlock title="默认/带有操作按钮" type="block">
-    <Tabs v-model:value="selectedTab" size="normal">
+    <Tabs v-model:value="selectedTab" size="large">
       <Tab name="all">全部</Tab>
       <Tab name="wiki">资料</Tab>
       <Tab name="community">社区</Tab>
@@ -10,11 +10,47 @@
       <Tab name="buy">求购</Tab>
       <Tab name="purchaseway">全网</Tab>
       <template #right>
-        <DuTabsRight>
-          <DuButton type="text" size="small" icon="sort" icon-position="right">测试1</DuButton>
-          <DuDivider />
-          <DuButton type="text" size="small" icon="sort" icon-position="right">测试2</DuButton>
-        </DuTabsRight>
+        <DuButton type="text" size="small" icon="sort" icon-position="right">测试1</DuButton>
+        <DuDivider />
+        <DuButton type="text" size="small" icon="sort" icon-position="right">测试2</DuButton>
+      </template>
+    </Tabs>
+  </PreviewBlock>
+  <PreviewBlock title="tag 类型" type="block">
+    <Tabs v-model:value="selectedTab" type="tag" size="large">
+      <Tab name="all">全部</Tab>
+      <Tab name="wiki">资料</Tab>
+      <Tab name="community">社区</Tab>
+      <Tab name="trade">转卖</Tab>
+      <Tab name="stock">闪购</Tab>
+      <Tab name="buy">求购</Tab>
+      <Tab name="purchaseway">全网</Tab>
+      <template #right>
+        <DuButton color="primary" type="text" size="small" icon="sort" icon-position="right">排序</DuButton>
+        <DuDivider />
+        <DuButton type="text" size="small" icon="sort" icon-position="right">测试2</DuButton>
+      </template>
+    </Tabs>
+  </PreviewBlock>
+  <PreviewBlock title="文字类型" type="block">
+    <Tabs v-model:value="selectedTab" type="text" size="large">
+      <Tab name="all">全部</Tab>
+      <DuDivider />
+      <Tab name="wiki">资料</Tab>
+      <DuDivider />
+      <Tab name="community">社区</Tab>
+      <DuDivider />
+      <Tab name="trade">转卖</Tab>
+      <DuDivider />
+      <Tab name="stock">闪购</Tab>
+      <DuDivider />
+      <Tab name="buy">求购</Tab>
+      <DuDivider />
+      <Tab name="purchaseway">全网</Tab>
+      <template #right>
+        <DuButton color="primary" type="text" size="small" icon="sort" icon-position="right">排序</DuButton>
+        <DuDivider />
+        <DuButton type="text" size="small" icon="sort" icon-position="right">测试2</DuButton>
       </template>
     </Tabs>
   </PreviewBlock>
@@ -22,7 +58,7 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import { Tabs, Tab, DuTabsRight, DuButton, DuDivider } from '@frontend/dumpling'
+import { Tabs, Tab, DuButton, DuDivider } from '@frontend/dumpling'
 
 const selectedTab = ref('all')
 </script>
--- docs/pages/demos/navigation/navigation-bar/snippet1.vue
+++ docs/pages/demos/navigation/navigation-bar/snippet1.vue
@@ -1,11 +1,51 @@
 
 <template>
   <div class="min-h-screen bg-white">
-    <div class="mb-8px">
-      <DuNavigationBar share>
+    <div class="mb-16px">
+      <DuNavigationBar share @share="handleShare">
         标题
         <template #right>
-          <div class="flex-auto w-0">
+          <DuButton size="small" type="info">按钮</DuButton>
+        </template>
+      </DuNavigationBar>
+    </div>
+    <div class="mb-16px">
+      <DuNavigationBar share @share="handleShare">
+        标题
+        <template #right>
+          <div class="flex-auto">
+            <DuSearch bg="white" :placeholder="placeholders" readonly>
+              <template #right>
+                <DuIcon name="camera" />
+                <DuDivider />
+                <DuIcon name="scanning" />
+              </template>
+            </DuSearch>
+          </div>
+        </template>
+      </DuNavigationBar>
+    </div>
+    <div class="mb-16px">
+      <DuNavigationBar share color="secondary" :back="false" @share="handleShare">
+        标题
+        <template #right>
+          <div class="flex-auto">
+            <DuSearch bg="white" placeholder="Labubu">
+              <template #right>
+                <DuIcon name="camera" />
+                <DuDivider />
+                <DuIcon name="scanning" />
+              </template>
+            </DuSearch>
+          </div>
+        </template>
+      </DuNavigationBar>
+    </div>
+    <div class="mb-16px">
+      <DuNavigationBar share color="primary" :back="false" @share="handleShare">
+        标题
+        <template #right>
+          <div class="flex-auto">
             <DuSearch bg="white" placeholder="Labubu">
               <template #right>
                 <DuIcon name="camera" />
@@ -21,12 +61,15 @@
 </template>
 
 <script setup lang="ts">
-import {
-  DuNavigationBar,
-  DuNavigationBarRight,
-  DuButton,
-  DuSearch,
-  DuIcon,
-  DuDivider,
-} from '@frontend/dumpling'
+import { DuNavigationBar, DuButton, DuSearch, DuIcon, DuDivider } from '@frontend/dumpling'
+
+const placeholders = [
+  'Molly',
+  'Labubu',
+  'Skullpanda'
+]
+
+function handleShare() {
+  console.log('[demo] share')
+}
 </script>
