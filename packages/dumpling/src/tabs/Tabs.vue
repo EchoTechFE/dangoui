4fa65d5dc8797ec4b487913f17d842dba7fd1945
zhuscat <zhudx6512@163.com>
2023-12-12 18:10:10 +0800

    feat: update plugin version

--- packages/dumpling/src/tabs/Tabs.vue
+++ packages/dumpling/src/tabs/Tabs.vue
@@ -34,7 +34,14 @@
 </template>
 
 <script setup lang="ts">
-import { computed, provide, toRef, ref, onMounted } from 'vue'
+import {
+  computed,
+  provide,
+  toRef,
+  ref,
+  onMounted,
+  getCurrentInstance,
+} from 'vue'
 import { TabsInjectionKey, getInstanceId } from './helpers'
 import { dividerInjectionKey } from '../divider/helpers'
 
@@ -62,14 +69,18 @@ const hasRight = ref(false)
 
 const scrollViewLeft = ref(0)
 
+const instance = getCurrentInstance()
+
 onMounted(() => {
-  const rightWrapperRect = document
-    .querySelector(`#${id} .du-tabs__right-wrapper`)
-    ?.getBoundingClientRect()
-  if (rightWrapperRect?.width && rightWrapperRect.width < 1) {
-    hasRight.value = false
-  } else {
-    hasRight.value = true
+  if (__WEB__) {
+    const rightWrapperRect = document
+      .querySelector(`#${id} .du-tabs__right-wrapper`)
+      ?.getBoundingClientRect()
+    if (rightWrapperRect?.width && rightWrapperRect.width < 1) {
+      hasRight.value = false
+    } else {
+      hasRight.value = true
+    }
   }
 })
 
@@ -80,27 +91,33 @@ const isScrollToEnd = computed(() => {
 
 function setValue(name: string, tabId: string) {
   emit('update:value', name)
-  // TODO:
-  const tabsEl = document.getElementById(id)
-  if (tabsEl) {
-    const scrollView = tabsEl.querySelector('.du-tabs__content-wrapper')
-    console.log(scrollView)
-    const tabEl = tabsEl.querySelector('#' + tabId)
-    console.log(tabEl)
-    const scrollViewRect = scrollView!.getBoundingClientRect()
-    const tabElRect = tabEl!.getBoundingClientRect()
+  if (__WEB__) {
+    const tabsEl = document.getElementById(id)
+    if (tabsEl) {
+      const scrollView = tabsEl.querySelector('.du-tabs__content-wrapper')
+      const tabEl = tabsEl.querySelector('#' + tabId)
+      const scrollViewRect = scrollView!.getBoundingClientRect()
+      const tabElRect = tabEl!.getBoundingClientRect()
 
-    let scrollLeft =
-      scrollView!.scrollLeft +
-      tabElRect.x +
-      tabElRect.width / 2 -
-      (scrollViewRect.x + scrollViewRect.width / 2)
+      let scrollLeft =
+        scrollView!.scrollLeft +
+        tabElRect.x +
+        tabElRect.width / 2 -
+        (scrollViewRect.x + scrollViewRect.width / 2)
 
-    if (scrollLeft < 0) {
-      scrollLeft = 0
+      if (scrollLeft < 0) {
+        scrollLeft = 0
+      }
+
+      scrollViewLeft.value = scrollLeft
     }
+  }
 
-    scrollViewLeft.value = scrollLeft
+  if (__UNI_PLATFORM__ !== 'h5') {
+    // @ts-ignore
+    const query = uni.createSelectorQuery().in(instance?.proxy)
+    query.select('.du-tabs__content-wrapper').boundingClientRect()
+    query.select('#' + tabId).boundingClientRect()
   }
 }
 
