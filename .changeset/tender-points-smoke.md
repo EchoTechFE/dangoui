3410f40d74024a07da744940332c9921e0663049
zhuscat <zhudx6512@163.com>
2024-01-23 19:10:08 +0800

    feat: 增加 tags panel 文档

---
+++ .changeset/tender-points-smoke.md
@@ -0,0 +1,5 @@
+---
+'@frontend/dumpling': patch
+---
+
+增加 tags panel 文档
--- packages/dumpling/src/tags-panel/TagsPanel.vue
+++ packages/dumpling/src/tags-panel/TagsPanel.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="du-tags-panel">
-    <DuButton size="small" type="outline" @click="$emit('add')">添加</DuButton>
+    <slot name="add">
+      <DuButton size="small" type="outline" @click="$emit('add')">
+        {{ addText }}
+      </DuButton>
+    </slot>
     <DuTag
       size="medium"
-      closeable
       v-for="tag in displayTags"
+      :closeable="'canRemove' in tag ? tag.canRemove : true"
       :key="tag.value"
       @close="$emit('remove', { value: tag.value })"
     >
       {{ tag.text }}
     </DuTag>
     <DuButton
-      v-if="!isExpanded && tags.length > 10"
+      v-if="(!isExpanded || canToggle) && tags.length > collapseCount"
       size="small"
       type="text"
       color="secondary"
-      icon="arrow-down-circle"
+      :icon="isExpanded ? 'arrow-up-circle' : 'arrow-down-circle'"
       icon-position="right"
       @click="handleExpand"
     >
-      展开 {{ tags.length }}
+      {{ toggleText }} {{ tags.length }}
     </DuButton>
   </div>
 </template>
@@ -31,10 +35,28 @@ import DuButton from '../button/Button.vue'
 
 const props = withDefaults(
   defineProps<{
-    tags: { text: string; value: string | number }[]
+    /**
+     * 添加按钮文案
+     */
+    addText: string
+    /**
+     * 标签，value 是唯一标识符，没有 canRemove 字段的话默认可以删除
+     */
+    tags: { text: string; value: string | number; canRemove?: boolean }[]
+    /**
+     * 可以收起
+     */
+    canToggle: boolean
+    /**
+     * 多少个的时候显示展开按钮
+     */
+    collapseCount: number
   }>(),
   {
+    addText: '添加',
     tags: () => [],
+    canToggle: false,
+    collapseCount: 10,
   },
 )
 
@@ -47,12 +69,17 @@ const displayTags = computed(() => {
   if (isExpanded.value) {
     return props.tags
   }
-  return props.tags.slice(0, 10)
+
+  return props.tags.slice(0, props.collapseCount)
 })
 
 const isExpanded = ref(false)
 
+const toggleText = computed(() => {
+  return isExpanded.value ? '收起' : '展开'
+})
+
 function handleExpand() {
-  isExpanded.value = true
+  isExpanded.value = !isExpanded.value
 }
 </script>
