e3f38f664e2df0fa8909adb5018410ffbb79fa43
zhuscat <zhudx6512@163.com>
2024-03-12 19:22:38 +0800

    feat: update tabs docs

--- docs/pages/demos/data-display/tabs/snippet4.vue
+++ docs/pages/demos/data-display/tabs/snippet4.vue
@@ -1,64 +1,54 @@
 
 <template>
-  <PreviewBlock title="默认/带有操作按钮" type="block">
-    <Tabs v-model:value="selectedTab" size="large">
-      <Tab name="all">全部</Tab>
-      <Tab name="wiki">资料</Tab>
-      <Tab name="community">社区</Tab>
-      <Tab name="trade">转卖</Tab>
-      <Tab name="stock">闪购</Tab>
-      <Tab name="buy">求购</Tab>
-      <Tab name="purchaseway">全网</Tab>
-      <template #right>
-        <DuButton type="text" size="small" icon="sort" icon-position="right">测试1</DuButton>
-        <DuDivider />
-        <DuButton type="text" size="small" icon="sort" icon-position="right">测试2</DuButton>
-      </template>
-    </Tabs>
+  <PreviewBlock title="secondary" type="block">
+    <DuTabs v-model:value="selectedTab" color="secondary" type="tag">
+      <DuTab name="all">全部</DuTab>
+      <DuTab name="wiki">资料</DuTab>
+      <DuTab name="community">社区</DuTab>
+      <DuTab name="trade">转卖</DuTab>
+      <DuTab name="stock">闪购</DuTab>
+      <DuTab name="buy">求购</DuTab>
+      <DuTab name="purchaseway">全网</DuTab>
+    </DuTabs>
   </PreviewBlock>
-  <PreviewBlock title="tag 类型" type="block">
-    <Tabs v-model:value="selectedTab" type="tag" size="large">
-      <Tab name="all">全部</Tab>
-      <Tab name="wiki">资料</Tab>
-      <Tab name="community">社区</Tab>
-      <Tab name="trade">转卖</Tab>
-      <Tab name="stock">闪购</Tab>
-      <Tab name="buy">求购</Tab>
-      <Tab name="purchaseway">全网</Tab>
-      <template #right>
-        <DuButton color="primary" type="text" size="small" icon="sort" icon-position="right">排序</DuButton>
-        <DuDivider />
-        <DuButton type="text" size="small" icon="sort" icon-position="right">测试2</DuButton>
-      </template>
-    </Tabs>
+  <PreviewBlock title="success" type="block">
+    <DuTabs v-model:value="selectedTab" color="success" type="tag">
+      <DuTab name="all">全部</DuTab>
+      <DuTab name="wiki">资料</DuTab>
+      <DuTab name="community">社区</DuTab>
+      <DuTab name="trade">转卖</DuTab>
+      <DuTab name="stock">闪购</DuTab>
+      <DuTab name="buy">求购</DuTab>
+      <DuTab name="purchaseway">全网</DuTab>
+    </DuTabs>
   </PreviewBlock>
-  <PreviewBlock title="文字类型" type="block">
-    <Tabs v-model:value="selectedTab" type="text" size="large">
-      <Tab name="all">全部</Tab>
-      <DuDivider />
-      <Tab name="wiki">资料</Tab>
-      <DuDivider />
-      <Tab name="community">社区</Tab>
-      <DuDivider />
-      <Tab name="trade">转卖</Tab>
-      <DuDivider />
-      <Tab name="stock">闪购</Tab>
-      <DuDivider />
-      <Tab name="buy">求购</Tab>
-      <DuDivider />
-      <Tab name="purchaseway">全网</Tab>
-      <template #right>
-        <DuButton color="primary" type="text" size="small" icon="sort" icon-position="right">排序</DuButton>
-        <DuDivider />
-        <DuButton type="text" size="small" icon="sort" icon-position="right">测试2</DuButton>
-      </template>
-    </Tabs>
+  <PreviewBlock title="warning" type="block">
+    <DuTabs v-model:value="selectedTab" color="warning" type="tag">
+      <DuTab name="all">全部</DuTab>
+      <DuTab name="wiki">资料</DuTab>
+      <DuTab name="community">社区</DuTab>
+      <DuTab name="trade">转卖</DuTab>
+      <DuTab name="stock">闪购</DuTab>
+      <DuTab name="buy">求购</DuTab>
+      <DuTab name="purchaseway">全网</DuTab>
+    </DuTabs>
+  </PreviewBlock>
+  <PreviewBlock title="warning" type="block">
+    <DuTabs v-model:value="selectedTab" color="error" type="tag">
+      <DuTab name="all">全部</DuTab>
+      <DuTab name="wiki">资料</DuTab>
+      <DuTab name="community">社区</DuTab>
+      <DuTab name="trade">转卖</DuTab>
+      <DuTab name="stock">闪购</DuTab>
+      <DuTab name="buy">求购</DuTab>
+      <DuTab name="purchaseway">全网</DuTab>
+    </DuTabs>
   </PreviewBlock>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import { Tabs, Tab, DuButton, DuDivider } from '@frontend/dumpling'
+import { DuTabs, DuTab, DuButton, DuDivider } from '@frontend/dumpling'
 
 const selectedTab = ref('all')
 </script>
