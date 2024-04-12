1c5057e6d88e4452b978953e4d8536058c1dd812
zhuscat <zhudx6512@163.com>
2024-04-12 17:43:12 +0800

    feat: 样式产物多一种 prefix 的，可以在一个项目中混合使用 rpx 模式与 px 模式

--- packages/dangoui/gulpfile.js
+++ packages/dangoui/gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const postcss = require('gulp-postcss')
+const through = require('through2')
 const px2rpx = require('./lib/px2rpx.cjs')
 
 // 定义编译 SCSS 任务
@@ -19,5 +20,31 @@ gulp.task('sass:mp', function () {
     .pipe(gulp.dest('dist/mp')) // 输出的 CSS 文件路径
 })
 
+function wrapCSS(prefix) {
+  function transform(file, encoding, callback) {
+    if (file.isStream()) {
+      this.emit('error', new Error('Streams are not supported!'));
+      return cb();
+    }
+
+    if (file.isBuffer()) {
+      file.contents = Buffer.concat([Buffer.from(`:where(.${prefix}) {\n`), file.contents, Buffer.from(`\n}`)])
+    }
+
+    this.push(file)
+    callback()
+  }
+
+  return through.obj(transform)
+}
+
+gulp.task('sass:prefix-px', function() {
+  return gulp.src('src/style.scss').pipe(sass()).pipe(wrapCSS('dangoui-prefix-px')).pipe(sass()).pipe(gulp.dest('dist/prefix/px'))
+})
+
+gulp.task('sass:prefix-rpx', function() {
+  return gulp.src('src/style.scss').pipe(sass()).pipe(wrapCSS('dangoui-prefix-rpx')).pipe(sass()).pipe(postcss([px2rpx()])).pipe(gulp.dest('dist/prefix/rpx'))
+})
+
 // 定义默认任务
-gulp.task('default', gulp.series(['sass', 'sass:mp']))
+gulp.task('default', gulp.series(['sass', 'sass:mp', 'sass:prefix-px', 'sass:prefix-rpx']))
