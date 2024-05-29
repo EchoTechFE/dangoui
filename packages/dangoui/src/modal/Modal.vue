b33c29a9f8dd60c8f474e79743766704c33a2587
443797825 <443797825@qq.com>
2024-05-29 11:42:33 +0800

    feat: add modal

--- packages/dangoui/src/modal/Modal.vue
+++ packages/dangoui/src/modal/Modal.vue
@@ -1,8 +1,12 @@
 <template>
+  <DuPopup v-model:visible="_visible">
 
+  </DuPopup>
 </template>
 <script lang="ts" setup>
 import DuPopup from '../popup/Popup.vue'
+import { ref } from 'vue'
 
+const _visible = ref(false)
 
 </script>
