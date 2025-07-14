b679888c9b44455212732fccb3a63103725aff11
zhangyanran <zhangyanran@echo.tech>
2025-07-14 17:27:01 +0800

    feat:use options

--- packages/dangoui/src/dropdown/Dropdown.vue
+++ packages/dangoui/src/dropdown/Dropdown.vue
@@ -1,23 +1,23 @@
 <template>
   <Popup v-model:visible="popupVisible" type="top" :closable="false">
     <div>
-      <!-- 筛选维度选择栏 -->
-      <div v-if="filterField.length > 1" class="du-dropdown__filter-fields">
+      <!-- 选项选择栏 -->
+      <div v-if="options.length > 1" class="du-dropdown__options-nav">
         <div
-          v-for="(field, index) in filterField"
-          :key="field.value"
-          :class="filterFieldClassName(index)"
-          @click="handleFilterFieldChange(index)"
+          v-for="(option, index) in options"
+          :key="option.value"
+          :class="optionNavClassName(index)"
+          @click="handleOptionNavChange(index)"
         >
-          <div>{{ field.label }}</div>
+          <div>{{ option.label }}</div>
           <DuIcon name="arrowdown" :size="8" />
         </div>
       </div>
-      <DuDivider v-if="filterField.length > 1" />
+      <DuDivider v-if="options.length > 1" />
 
       <!-- 选项内容区 -->
       <div class="du-dropdown__content">
-        <template v-if="currentFilterField">
+        <template v-if="currentOption">
           <div
             v-for="group in currentGroups"
             :key="group.value"
@@ -113,9 +113,9 @@ export type SelectedValue = Record<string, any>
 const props = withDefaults(
   defineProps<{
     /**
-     * 筛选维度配置
+     * 选项配置
      */
-    filterField: FilterField[]
+    options: FilterField[]
     /**
      * 当前选中的值
      */
@@ -150,7 +150,7 @@ const emit = defineEmits<{
 }>()
 
 const internalValue = ref<SelectedValue>({})
-const currentFilterFieldIndex = ref(0)
+const currentOptionIndex = ref(0)
 
 watch(
   () => props.visible,
@@ -158,7 +158,7 @@ watch(
     if (newValue) {
       internalValue.value = JSON.parse(JSON.stringify(props.value))
     } else {
-      currentFilterFieldIndex.value = 0
+      currentOptionIndex.value = 0
     }
   },
 )
@@ -172,66 +172,64 @@ const popupVisible = computed({
   },
 })
 
-const currentFilterField = computed(
-  () => props.filterField[currentFilterFieldIndex.value],
-)
+const currentOption = computed(() => props.options[currentOptionIndex.value])
 
 const currentGroups = computed(() => {
-  const field = currentFilterField.value
-  if (!field) return []
-  if ('options' in field) {
+  const option = currentOption.value
+  if (!option) return []
+  if ('options' in option) {
     return [
       {
-        label: field.label,
-        value: field.value,
-        multiple: field.multiple,
-        options: field.options,
+        label: option.label,
+        value: option.value,
+        multiple: option.multiple,
+        options: option.options,
       },
     ]
   }
-  return field.groups
+  return option.groups
 })
 
 function isSelected(option: DropdownOption, group: OptionGroup): boolean {
-  const field = currentFilterField.value
-  if (!field) return false
+  const currentOpt = currentOption.value
+  if (!currentOpt) return false
 
-  const fieldValue = internalValue.value[field.value]
-  if (!fieldValue) return false
+  const optionValue = internalValue.value[currentOpt.value]
+  if (!optionValue) return false
 
-  if (Array.isArray(fieldValue)) {
-    return fieldValue.some((opt: DropdownOption) => opt.value === option.value)
+  if (Array.isArray(optionValue)) {
+    return optionValue.some((opt: DropdownOption) => opt.value === option.value)
   }
 
-  const groupOptions = fieldValue[group.value]
+  const groupOptions = optionValue[group.value]
   if (!groupOptions) return false
 
   return groupOptions.some((opt: DropdownOption) => opt.value === option.value)
 }
 
 function handleSelect(option: DropdownOption, group: OptionGroup) {
-  const field = currentFilterField.value
-  if (!field) return
+  const currentOpt = currentOption.value
+  if (!currentOpt) return
 
-  const fieldValue = field.value
+  const optionValue = currentOpt.value
 
-  const isMultiple = 'multiple' in group ? group.multiple : field.multiple
+  const isMultiple = 'multiple' in group ? group.multiple : currentOpt.multiple
 
-  if ('options' in field) {
+  if ('options' in currentOpt) {
     // 处理直接有options的情况
     let currentOptions: DropdownOption[]
 
     if (
-      !internalValue.value[fieldValue] ||
-      !Array.isArray(internalValue.value[fieldValue])
+      !internalValue.value[optionValue] ||
+      !Array.isArray(internalValue.value[optionValue])
     ) {
       currentOptions = []
       internalValue.value = {
         ...internalValue.value,
-        [fieldValue]: currentOptions,
+        [optionValue]: currentOptions,
       }
     } else {
-      currentOptions = internalValue.value[fieldValue]
+      currentOptions = internalValue.value[optionValue]
     }
 
     const existingIndex = currentOptions.findIndex(
@@ -245,30 +243,30 @@ function handleSelect(option: DropdownOption, group: OptionGroup) {
         currentOptions.push(option)
       }
     } else {
-      internalValue.value[fieldValue] = [option]
+      internalValue.value[optionValue] = [option]
     }
   } else {
     // 处理有groups的情况
-    let filterValue: Record<string, DropdownOption[]>
+    let optionsValue: Record<string, DropdownOption[]>
 
     if (
-      !internalValue.value[fieldValue] ||
-      Array.isArray(internalValue.value[fieldValue])
+      !internalValue.value[optionValue] ||
+      Array.isArray(internalValue.value[optionValue])
     ) {
-      filterValue = {}
+      optionsValue = {}
       internalValue.value = {
         ...internalValue.value,
-        [fieldValue]: filterValue,
+        [optionValue]: optionsValue,
       }
     } else {
-      filterValue = internalValue.value[fieldValue]
+      optionsValue = internalValue.value[optionValue]
     }
 
-    if (!filterValue[group.value]) {
-      filterValue[group.value] = []
+    if (!optionsValue[group.value]) {
+      optionsValue[group.value] = []
     }
 
-    const groupOptions = filterValue[group.value]
+    const groupOptions = optionsValue[group.value]
     const existingIndex = groupOptions.findIndex(
       (opt) => opt.value === option.value,
     )
@@ -280,14 +278,14 @@ function handleSelect(option: DropdownOption, group: OptionGroup) {
         groupOptions.push(option)
       }
     } else {
-      filterValue[group.value] = [option]
+      optionsValue[group.value] = [option]
     }
   }
   internalValue.value = { ...internalValue.value }
 }
 
-function handleFilterFieldChange(index: number) {
-  currentFilterFieldIndex.value = index
+function handleOptionNavChange(index: number) {
+  currentOptionIndex.value = index
 }
 
 function handleCancel() {
@@ -298,18 +296,18 @@ function handleCancel() {
 function handleConfirm() {
   const selectedOptions: SelectedValue = {}
 
-  props.filterField.forEach((field) => {
-    const fieldValue = field.value
-    const filterValue = internalValue.value[fieldValue] || {}
+  props.options.forEach((option) => {
+    const optionValue = option.value
+    const filterValue = internalValue.value[optionValue] || {}
 
     // 处理直接有options的情况
-    if ('options' in field) {
+    if ('options' in option) {
       const options = filterValue as DropdownOption[]
-      selectedOptions[fieldValue] = options || []
+      selectedOptions[optionValue] = options || []
     } else {
       // 处理有groups的情况
       const groupedValue = filterValue as Record<string, DropdownOption[]>
-      selectedOptions[fieldValue] = groupedValue
+      selectedOptions[optionValue] = groupedValue
     }
   })
   popupVisible.value = false
@@ -318,23 +316,22 @@ function handleConfirm() {
 }
 
 const hasSelected = computed(() => {
-  const fieldValues = internalValue.value || {}
-  return Object.values(fieldValues).some((fieldValue) => {
-    if (Array.isArray(fieldValue)) {
-      return fieldValue.length > 0
+  const optionValues = internalValue.value || {}
+  return Object.values(optionValues).some((optionValue) => {
+    if (Array.isArray(optionValue)) {
+      return optionValue.length > 0
     }
-    return Object.values(fieldValue).some(
+    return Object.values(optionValue).some(
       (groupValue) => Array.isArray(groupValue) && groupValue.length > 0,
     )
   })
 })
 
-function filterFieldClassName(index: number) {
+function optionNavClassName(index: number) {
   return normalizeClass([
-    'du-dropdown__filter-field',
+    'du-dropdown__option-nav',
     {
-      'du-dropdown__filter-field--active':
-        currentFilterFieldIndex.value === index,
+      'du-dropdown__option-nav--active': currentOptionIndex.value === index,
     },
   ])
 }
